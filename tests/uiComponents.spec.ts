ecda9afb2384eb67a53fd47146e0ce47d4c1b164
Juan Pablo HM <juanpablohm1995@gmail.com>
2025-09-01 14:16:01 -0600

    Checkboxes Lesson

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,4 +1,4 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -16,7 +16,7 @@ test.describe('Form Layouts page', () => {
 
         await usingTheGridEmailInput.fill('test@test.com')
         await usingTheGridEmailInput.clear()
-        await usingTheGridEmailInput.pressSequentially('test2@newtest.com', {delay: 500})
+        await usingTheGridEmailInput.pressSequentially('test2@newtest.com', { delay: 500 })
 
         //Generic Assertion:
         const inputValue = await usingTheGridEmailInput.inputValue()
@@ -26,23 +26,36 @@ test.describe('Form Layouts page', () => {
         await expect(usingTheGridEmailInput).toHaveValue('test2@newtest.com')
     })
 
-    test('Radio Buttons', async ({page}) => {
+    test('Radio Buttons', async ({ page }) => {
         const usingTheGridEmailInput = page.locator('nb-card', { hasText: "Using the Grid" })
-            
 
-            //await usingTheGridEmailInput.getByLabel('Option 1').check({force: true})
-            await usingTheGridEmailInput.getByRole('radio', {name: "Option 1"}).check({force: true})
-            const radioSatuts = await usingTheGridEmailInput.getByRole('radio', {name: "Option 1"}).isChecked()
-            expect(radioSatuts).toBeTruthy()
-            await expect(usingTheGridEmailInput.getByLabel('Option 1')).toBeChecked()
 
-            await usingTheGridEmailInput.getByRole('radio', {name: "Option 2"}).check({force: true})
-            expect(await usingTheGridEmailInput.getByRole('radio', {name: "Option 1"}).isChecked()).toBeFalsy()
-            expect(await usingTheGridEmailInput.getByRole('radio', {name: "Option 2"}).isChecked()).toBeTruthy()
-            
+        //await usingTheGridEmailInput.getByLabel('Option 1').check({force: true})
+        await usingTheGridEmailInput.getByRole('radio', { name: "Option 1" }).check({ force: true })
+        const radioSatuts = await usingTheGridEmailInput.getByRole('radio', { name: "Option 1" }).isChecked()
+        expect(radioSatuts).toBeTruthy()
+        await expect(usingTheGridEmailInput.getByLabel('Option 1')).toBeChecked()
+
+        await usingTheGridEmailInput.getByRole('radio', { name: "Option 2" }).check({ force: true })
+        expect(await usingTheGridEmailInput.getByRole('radio', { name: "Option 1" }).isChecked()).toBeFalsy()
+        expect(await usingTheGridEmailInput.getByRole('radio', { name: "Option 2" }).isChecked()).toBeTruthy()
+
 
 
     })
+})
 
+test('checkboxes', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
 
+    await page.getByRole('checkbox', {name: "Hide on click"}).check({force: true})
+    await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).uncheck({force: true})
+
+    const allBoxes = page.getByRole('checkbox')
+    for (const box of await allBoxes.all()){
+        await box.uncheck({force: true})
+        expect(await box.isChecked()).toBeFalsy()
+
+    }
 })
