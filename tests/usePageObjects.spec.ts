1680adb80ef28672d5ebddc7a414d9a48ad39ecf
Juan Pablo HM <juanpablohm1995@gmail.com>
2025-11-04 06:51:20 -0600

    Date Picker methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test'
 import { NavigationPage } from '../page-objects/navigationPage'
 import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
+import { DatepickerPage } from '../page-objects/datepickerPage'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -18,8 +19,12 @@ test('Navigate to form page', async ({page}) =>  {
 test('Parametrize Methods', async({page}) =>{
     const navigateTo = new NavigationPage(page)
     const onFormLayoutsPage = new FormLayoutsPage(page)
+    const onDatePickerDate = new DatepickerPage(page)
 
     await navigateTo.formLayoutsPage()
     await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption("test@tes.com", "Pass123", "Option 2")
     await onFormLayoutsPage.submitInLineFormWithNameEmailAndCheckbox("JP", "jp@test.com", false)
+    await navigateTo.datePickerPage()
+    await onDatePickerDate.selectCommonDatePickerDateFromToday(20)
+    await onDatePickerDate.selectDatePickerWithRangeFromToday(6, 15)
 })
